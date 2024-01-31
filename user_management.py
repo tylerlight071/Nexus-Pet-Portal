3c23cd3186177de065f57edff7b2996a58908317
Tyler Lightwood <tylerlightwood071@gmail.com>
2024-01-31 16:01:29 +0000

    Fixed ADMIN issues
    - Fixed issue where ADMIN commands returned save_file errors due to the function looking for users.json so modified to link to database rather than use JSON file
    - Changed 'change user password' to 'reset_user_password' as that is the new function as it changes the users password to password for them to change

--- user_management.py
+++ user_management.py
@@ -1,8 +1,15 @@
 import time
 from colorama import Fore, Style
-from common_functions import clear_screen, load_data, save_data
+from common_functions import clear_screen, generate_salt, hash_password
+from pymongo import MongoClient
+from config import mongodb_uri
 
-USER_DATA_FILE = "users.json"
+# Connect to MongoDB
+uri = mongodb_uri
+client = MongoClient(uri)
+db = client['animal_rescue']
+users_collection = db['users']
+
 
 def user_management():
     # Continuous loop for user management options
@@ -17,7 +24,7 @@ def user_management():
 
         # Perform actions based on user input
         if option == '1':
-            change_user_password()
+            reset_user_password()
         elif option == '2':
             update_user_information()
         elif option == '3':
@@ -30,35 +37,43 @@ def user_management():
             print(Fore.RED + "\nInvalid option. Please try again." + Style.RESET_ALL)
             time.sleep(2)
 
-def change_user_password():
-    # Load user data and prompt for username and new password
-    users = load_data(USER_DATA_FILE)
+def reset_user_password():
+    # Prompt for username and new password
     username = input("\nEnter the username to change the password: ")
 
-    # Change password if the user exist and is not ADMIN
-    if username in users and username != "ADMIN": 
-        new_password = input("Enter the new password (use password to set their own): ")
-        users[username]['password'] = new_password
-        save_data(users, USER_DATA_FILE)
-        print(Fore.GREEN + f"\nPassword for user '{username}' changed successfully!" + Style.RESET_ALL)
+    # Change password if the user exists and is not ADMIN
+    user = users_collection.find_one({'username': username})
+    if user and username != "ADMIN": 
+        new_password = "password"
+
+        # Generate salt and hash password
+        salt = generate_salt()
+        hashed_password = hash_password(new_password, salt)
+
+        # Convert salt to hexadecimal string for serialization
+        salt_hex = salt.hex()
+        
+        # Update user's password and salt
+        users_collection.update_one({'username': username}, 
+                                    {'$set': {'hashed_password': hashed_password, 'salt': salt_hex}})
+        print(Fore.GREEN + f"\nPassword for user '{username}' reset successfully!" + Style.RESET_ALL)
+    
     elif username == "ADMIN":  
         print(Fore.RED + "\nYou cannot change the password for the ADMIN user." + Style.RESET_ALL)
+    
     else:
         print(Fore.RED + f"\nUser '{username}' not found." + Style.RESET_ALL)
-
     time.sleep(2)
 
 def update_user_information():
-    # Load user data and prompt for username and updates
-    users = load_data(USER_DATA_FILE)
-    clear_screen()
+    # Prompt for username to update information
     username = input("\nEnter the username to update information: ")
 
-    if username in users and username != "ADMIN":  
-        clear_screen()
+    user = users_collection.find_one({'username': username})
+    if user and username != "ADMIN":  
         print("\nCurrent user information:")
         print(f"\nUsername: {username}")
-        print(f"User Level: {users[username]['level']}")
+        print(f"User Level: {user['level']}")
         
         # Prompt for the information to be updated
         print("\nSelect the information you want to update:")
@@ -71,14 +86,14 @@ def update_user_information():
         # Process user's choice and perform updates accordingly
         if option == '1':
             new_username = input("Enter the new username: ")
-            if new_username not in users:  
-                users[new_username] = users.pop(username)
+            if not users_collection.find_one({'username': new_username}):  
+                users_collection.update_one({'username': username}, {'$set': {'username': new_username}})
                 print(Fore.GREEN + f"\nUsername updated successfully to '{new_username}'!" + Style.RESET_ALL)
             else:
                 print(Fore.RED + f"\nUsername '{new_username}' already exists. Please choose a different username." + Style.RESET_ALL)
         elif option == '2':
             new_level = int(input("Enter the new user level: "))
-            users[username]['level'] = new_level
+            users_collection.update_one({'username': username}, {'$set': {'level': new_level}})
             print(Fore.GREEN + f"\nUser level updated successfully for '{username}'!" + Style.RESET_ALL)
         elif option == '3':
             print("\nOperation canceled.")
@@ -89,12 +104,10 @@ def update_user_information():
     else:
         print(Fore.RED + f"\nUser '{username}' not found." + Style.RESET_ALL)
 
-    save_data(users, USER_DATA_FILE)
     time.sleep(2)
 
 def delete_user():
-    # Load user data and prompt for username to be deleted
-    users = load_data(USER_DATA_FILE)
+    # Prompt for username to be deleted
     username = input("\nEnter the username to delete: ")
     confirm_delete = input("Are you sure you want to delete this user? (y/n) ")
 
@@ -107,11 +120,11 @@ def delete_user():
     
     # Process user's choice and perform deletion accordingly
     else:
-        if username in users and confirm_delete.lower() == 'y':
-            del users[username]
-            save_data(users, USER_DATA_FILE)
+        user = users_collection.find_one({'username': username})
+        if user and confirm_delete.lower() == 'y':
+            users_collection.delete_one({'username': username})
             print(Fore.GREEN + f"\nUser '{username}' deleted successfully!" + Style.RESET_ALL)
-        elif username in users and confirm_delete.lower() == 'n':
+        elif user and confirm_delete.lower() == 'n':
             print(Fore.RED + "User has not been deleted!" + Style.RESET_ALL)
         else:
             print(Fore.RED + f"\nUser '{username}' not found." + Style.RESET_ALL)
