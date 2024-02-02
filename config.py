a87f16991cd1ff508d08af2ef2da8a13be4da148
Tyler Lightwood <tylerlightwood071@gmail.com>
2024-02-02 10:54:52 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,1 @@
-mongodb_uri = 'mongodb+srv://tylerlightwood071:ARZUitkHYYxSySeT@cluster0.wa3y3z3.mongodb.net/?retryWrites=true&w=majority'
-# URI Inputted
+mongodb_uri = 'mongodb://localhost:27017/'
