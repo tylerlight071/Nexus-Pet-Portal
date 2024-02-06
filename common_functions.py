1020a8eb8440818b8ef9f957b8a7620ea6291aa5
Tyler Lightwood <tylerlightwood071@gmail.com>
2024-02-06 13:31:49 +0000

    Updated UI
    - Modified the ui for better user experience

--- common_functions.py
+++ common_functions.py
@@ -2,6 +2,7 @@ import os
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 import datetime
+import configparser
 
 def clear_screen():
     # Clears the terminal screen based on the OS type
@@ -46,7 +47,7 @@ def log_action(username, action_description):
 
 def hash_password(password: str):
     # Hash the password using Argon2
-    ph = PasswordHasher(time_cost=2, memory_cost=102400, parallelism=8, hash_len=16, salt_len=16, encoding='utf-8')
+    ph = PasswordHasher(time_cost=10, memory_cost=409600, parallelism=16, hash_len=256, salt_len=256, encoding='utf-8')
     hashed_password = ph.hash(password)
     return hashed_password
 
@@ -61,5 +62,16 @@ def verify_password(stored_password, entered_password):
     except VerifyMismatchError:
         return False
     
-def generate_salt():
-    pass
+def get_mongodb_uri():
+    # Read the configuration file
+    config = configparser.ConfigParser()
+    config_file = 'config.ini'
+    if os.path.exists(config_file):
+        config.read(config_file)
+        return config['DEFAULT']['mongodb_uri']
+    else:
+        uri = input("Please enter your MongoDB URI: ")
+        config['DEFAULT'] = {'mongodb_uri': uri}
+        with open(config_file, 'w') as configfile:
+            config.write(configfile)
+        return uri
